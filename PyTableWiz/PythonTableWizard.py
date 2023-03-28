c51b25ef8f3e852328f0e9fcc6fbf0adc86f42a5
PramudithRangana <pramudithrangana@gmail.com>
2023-03-28 17:52:03 +0530

    Add Reading details function and some features.

--- PyTableWiz/PythonTableWizard.py
+++ PyTableWiz/PythonTableWizard.py
@@ -1,27 +1,23 @@
 import csv
+from timeit import default_timer as timer
 
 """ Developer   : Pramu Programming Concept """
 """ Description : This program has been created for represent (csv, json etc..) data file as a table. """
-""" Date        : Wednesday, 01 March 2023 """
+""" Date        : Thursday, 16 March 2023 """
 
 
 class TableWizard:
-    def __init__(self, file_path, dialect_mode="r", limit=100, justify='left', align=None, acc_details=True,
-                 view_attr=None,
+    def __init__(self, file_path, dialect_mode="r", limit=100, justify='left',
+                 align=None, acc_details=True, DTE=None,
                  tl="┌", ti="┬", tr="┐", hdl="├", hdi="┼", hdr="┤", bl="└", bi="┴", br="┘", vl="│", hl="─"):
 
         self.file_path = file_path
         self.dialect_mode = dialect_mode
-        self.index_finder = []
-        self.header = None
-        self.row_count = 0
-        self.file_path = file_path
-        self.file_name = file_path.split('/')[-1]
         self.limit = limit
         self.justify = justify
         self.align = align  # need in dictionary format
         self.accessories_details = acc_details
-        self.view_attributes = view_attr
+        self.Derived_table = DTE  # need in list format
 
         # adjustments
         self.top_left = tl
@@ -36,14 +32,31 @@ class TableWizard:
         self.vertical_line = vl
         self.horizontal_line = hl
 
+        self.col_index = None
+        self.row_count = 0
+
         self.temp_data_list = []
         self.data_list = []
         self.col_width = []
 
+        # star time of entire data table creating
+        self.start = timer()
+        # Read data from (csv, ..) data file and get all
         self.dataReader()
+        # set maximum column width according to the content of each column
         self.column_width()
+        # call the table creator
         self.draw_table()
+        # end time of entire data table creating
+        self.stop = timer()
 
+        # call to data Read Time Counting function
+        if self.accessories_details:
+            self.reading_details()
+        else:
+            pass
+
+    # get all data
     def dataReader(self):
         # get file path
         if self.file_path:
@@ -51,17 +64,24 @@ class TableWizard:
                 with open(self.file_path, self.dialect_mode) as f:
                     csvRead = csv.reader(f)
 
-                    if self.view_attributes is None:
+                    if self.Derived_table is None:
                         for row in csvRead:
+                            # append data for (normal) table creation
                             self.data_list.append(row)
                         return self.data_list
 
                     else:
+                        # Derived Table Expression
                         for row in csvRead:
+                            # store table data temporary
                             self.temp_data_list.append(row)
-                            col_index = [self.temp_data_list[0].index(x) for x in self.view_attributes if
-                                         x in self.temp_data_list[0]]
-                            self.data_list.append(list(row[i] for i in col_index))
+                            # find index number of each column by headers
+                            if not self.col_index:
+                                # find index number of each column by headers(column name)
+                                self.col_index = [self.temp_data_list[0].index(x) for x in self.Derived_table if
+                                                  x in self.temp_data_list[0]]
+                            # append data for (derived) table creation
+                            self.data_list.append(list(row[i] for i in self.col_index))
 
                         return self.data_list
             except Exception as e:
@@ -123,11 +143,12 @@ class TableWizard:
         # find the column numbers with stick position to Set alignment to particular column(s)
         reset_align = self.justify
         if self.align is None:
+            # Default alignment representation
             pass
         else:
             try:
-                # Set index no. instead of column name to the dictionary
-                self.align = {self.header.index(x): y for x, y in self.align.items()}
+                # Set index no. instead of column name to the dictionary ---> N.B: self.data_list[0] (header)
+                self.align = {self.data_list[0].index(x): y for x, y in self.align.items() if x in self.data_list[0]}
             except ValueError as ve:
                 print(f"[!] ValueError : {ve}\n Please check your 'alignment' argument")
                 exit()
@@ -138,16 +159,17 @@ class TableWizard:
         self.draw_separators(self.top_left, self.top_interconnection, self.top_right)
         print(f"{self.vertical_line} ", end=" ")
 
-        # self.header of the entity
+        # self.header of the entity ---> N.B: self.data_list[0] (header)
         for column in enumerate(self.data_list[0]):
             print(self.fixed_length(column[1], self.col_width[column[0]]), end=f"  {self.vertical_line}  ")
         print()
-        # table body separation from the self.header
+        # table body separation from the header
         self.draw_separators(self.heading_left, self.heading_interconnection, self.heading_right)
-        # header dropping------------------------------------------------------------------------------------->
-        self.header = self.data_list.pop(0)
 
-        # body of the entity
+        # header dropping--------------------------->>
+        self.data_list.pop(0)
+
+        # body of the entity -------> N.B: self.data_list[1:] (content)
         for row in enumerate(self.data_list):
             # row range limitation
             if self.row_count == self.limit:
@@ -172,6 +194,17 @@ class TableWizard:
 
         # end of the table
         self.draw_separators(self.bottom_left, self.bottom_interconnection, self.bottom_right)
+
+    # Accessory Details
+    def reading_details(self):
+        print(f"👉 Records    : {self.row_count}") if self.row_count > 1 else print(f"👉 Record     : {self.row_count}")
+        print(f"👉 Attributes : {len(self.data_list[0])}") if len(self.data_list[0]) > 1 else \
+            print(f"👉 Attribute  : {len(self.data_list[0])}")
+        print(f"👉 File Name  : {self.file_path.split('/')[-1]}")
+
+        # Total Time to Effect (Time Consume)
+        time_duration = (self.stop - self.start)
+        print(f"👉 TTE        : {time_duration:.4f} sec")
 
 # if __name__ == '__main__':
 #     TableWizard("src/test_4.csv")
